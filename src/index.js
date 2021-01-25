751af4e31d61af0d3f4e28ba5c68db721b6aa2cf
sherrylin428 <68943008+sherrylin428@users.noreply.github.com>
2021-01-25 12:21:39 +0800

    Delete src directory

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './Style/home.css';
-import Searchinput from './Item/searchInput';
-
-ReactDOM.render(
-    <Searchinput/>,document.getElementById("root")
-); 
